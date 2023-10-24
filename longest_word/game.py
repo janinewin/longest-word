b4e960bf4c0f20a758f83d02a6eed4bcae437fd4
Janine Windhoff <janine.windhoff@proton.me>
2023-10-24 19:22:47 +0000

    Game developmet with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,33 @@
+# pylint: disable=missing-docstring
+
+import string
+import random
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        if word == "":
+            return False
+
+        letters = self.grid.copy()
+
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+
+        return True
+
+
+if __name__ == "__main__":
+    game = Game()
+    print(game.grid) # --> OQUWRBAZE
+    my_word = "BAROQUE"
+    game.is_valid(my_word) # --> True
---
+++ tests/test_game.py
@@ -0,0 +1,58 @@
+from longest_word.game import Game
+import string
+
+class TestGame:
+    def test_game_initialization(self):
+        # setup
+        game = Game()
+
+        # exercise
+        grid = game.grid
+
+        # verify
+        assert type(grid) == list
+        assert len(grid) == 9
+        for letter in grid:
+            assert letter in string.ascii_uppercase
+
+        # teardown
+
+
+    def test_is_valid(self):
+        #setup
+        game = Game()
+        test_grid = "KLSWTRXZO"
+        test_word = "SLOW"
+
+        #exercise
+        game.grid = list(test_grid)
+
+        #verfiy
+        assert game.is_valid(test_word) is True
+
+        #teardown
+        assert game.grid == list(test_grid)
+
+
+    def test_is_invalid(self):
+        #setup
+        game = Game()
+        test_grid = "KLSWTRXZO"
+        test_word = "TROTT"
+
+        #exercise
+        game.grid = list(test_grid)
+
+        #verify
+        assert game.is_valid(test_word) is False
+
+        #teardown
+        assert game.grid == list(test_grid)
+
+
+    def test_empty_is_invalid(self):
+        #setup
+        game = Game()
+
+        #verify
+        assert game.is_valid("") is False
